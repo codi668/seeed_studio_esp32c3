56d21493be774d09505d72489f7b71f8e9a0a2ea
codi668 <20220061@htlstp.at>
2025-05-14 12:25:41 +0200

    https_client

--- src/main.cpp
+++ src/main.cpp
@@ -27,53 +27,18 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <LittleFS.h>
+#include <ESPAsyncWebServer.h>
+#include <esp_bt.h>
+
 //#include "credentials.h"
 
-const char* ssid = "***************";
-const char* password = "*****************";
+const char* ssid = "HTL-WLAN-IoT";
+const char* password = "HTL2IoT!";
 
 #define LED_PIN 2
 
-WebServer server(80);
+AsyncWebServer server(80);
 
-void handleRoot() {
-    Serial.println("Handling root request");
-    File file = LittleFS.open("/index.html", "r");
-    if (!file) {
-        Serial.println("Failed to open index.html");
-        server.send(500, "text/plain", "Failed to open index.html");
-        return;
-    }
-    String html = file.readString();
-    server.send(200, "text/html", html);
-    file.close();
-    Serial.println("Root request handled successfully");
-}
-
-void handleLEDOn() {
-    Serial.println("Turning LED on");
-    digitalWrite(LED_PIN, HIGH);
-    server.send(200, "text/plain", "LED is ON");
-    Serial.println("LED is ON");
-}
-
-void handleLEDOff() {
-    Serial.println("Turning LED off");
-    digitalWrite(LED_PIN, LOW);
-    server.send(200, "text/plain", "LED is OFF");
-    Serial.println("LED is OFF");
-}
-void handleImage() {
-    Serial.println("Handling image request");
-    File file = LittleFS.open("/grubar_trans.webp", "r");
-    if (!file) {
-        server.send(500, "text/plain", "Failed to open Grubar.jpg");
-        return;
-    }
-    server.streamFile(file, "image/webp");
-    file.close();
-    Serial.println("Image request handled successfully");
-}
 
 void setup() {
     Serial.begin(115200);
@@ -97,13 +62,24 @@ void setup() {
     Serial.println("\nIP address: ");
     Serial.println(WiFi.localIP());
 
-    server.on("/", handleRoot);
-    server.on("/led/on", handleLEDOn);
-    server.on("/led/off", handleLEDOff);
-    server.on("/logo",handleImage);
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(LittleFS, "/index.html", "text/html");
+    });
+    server.on("/led/on", HTTP_GET, [](AsyncWebServerRequest *request) {
+        digitalWrite(LED_PIN, HIGH);
+        request->send(LittleFS, "/index.html", "text/html");
+    });
+    server.on("/led/off", HTTP_GET, [](AsyncWebServerRequest *request) {
+        digitalWrite(LED_PIN, LOW);
+        request->send(LittleFS, "/index.html", "text/html");
+    });
+
+    server.on("/logo", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(LittleFS, "/grubar_trans.webp", "image/webp");
+    });
+
     server.begin();
     Serial.println("HTTP server started");
 }
 void loop() {
-    server.handleClient();
 }
